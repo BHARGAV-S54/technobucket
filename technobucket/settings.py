86bcd40c595d4da2c833d0d89e9b5d72fbc3f594
bhargav <sanaboyinabhargav@gmail.com>
2026-03-31 20:14:07 +0530

    feat: Update STATIC_URL and MEDIA_URL paths; configure WhiteNoise for static file handling

--- technobucket/settings.py
+++ technobucket/settings.py
@@ -98,14 +98,19 @@ TIME_ZONE = 'Asia/Kolkata'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# WhiteNoise compression and caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
+WHITENOISE_USE_FINDERS = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
