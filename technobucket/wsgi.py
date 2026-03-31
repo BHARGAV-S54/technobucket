ba7fa6ec9af403d4cd6f50ef79abe191bf326694
bhargav <sanaboyinabhargav@gmail.com>
2026-03-31 22:01:40 +0530

    wn4

--- technobucket/wsgi.py
+++ technobucket/wsgi.py
@@ -9,10 +9,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "technobucket.settings")
 
 django.setup()
 
-from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
-
-if not os.environ.get("DEBUG"):
-    call_command("collectstatic", "--noinput", verbosity=0)
 
 application = get_wsgi_application()
